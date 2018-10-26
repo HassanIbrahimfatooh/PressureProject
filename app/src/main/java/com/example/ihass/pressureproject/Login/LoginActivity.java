b593bf3054c7d624fab99ac0c21c56bb37c06ec0
Hassan Ibrahim <hassani49@h-eng.helwan.edu.eg>
2018-10-26 19:49:46 +0200

    login application second commit with no added pages but firebase issues resolved

---
+++ app/src/main/java/com/example/ihass/pressureproject/Login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.ihass.pressureproject.Login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ihass.pressureproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("LogIn");
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/ihass/pressureproject/MainActivity.java
+++ app/src/main/java/com/example/ihass/pressureproject/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.ihass.pressureproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ihass.pressureproject.Login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Main");
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
