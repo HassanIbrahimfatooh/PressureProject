ee277dadb18865fba4997c08decb1c1e41b48d56
Hassan Ibrahim <hassani49@h-eng.helwan.edu.eg>
2018-11-04 11:47:42 +0200

    Commit 9: Implementing nav bar and slide view page. There is a problem: Need to run in internet.

--- app/src/main/java/com/example/ihass/pressureproject/Implementation/MainView.java
+++ app/src/main/java/com/example/ihass/pressureproject/Implementation/MainView.java
@@ -1,16 +1,79 @@
 package com.example.ihass.pressureproject.Implementation;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.ihass.pressureproject.R;
 
-public class MainView extends AppCompatActivity {
+public class MainView extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Main View");
+        setTitle("Main");
         setContentView(R.layout.activity_main_view);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // TODO : Implement this function as required.
+        // I don't know the Required ^_^
+        SetUpViewPager();
+    }
+
+    private void SetUpViewPager() {
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // set item as selected to persist highlight
+        menuItem.setChecked(true);
+        // close drawer when item is tapped
+
+        // Add code here to update the UI based on the item selected
+        // For example, swap UI fragments here
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Toast.makeText(getApplicationContext(), "Nav Bar clicked", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_gallery) {
+            Toast.makeText(getApplicationContext(), "Gallery clicked", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_slideshow) {
+            Toast.makeText(getApplicationContext(), "Slide Show clicked", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_manage) {
+            Toast.makeText(getApplicationContext(), "Manage clicked", Toast.LENGTH_LONG).show();
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
